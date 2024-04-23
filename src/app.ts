fa3ebd0d4282cf964e0d24d99c7da6de013ac577
Snaflus <casp909i@edu.easj.dk>
2024-04-23 13:34:31 +0200

    post promotion functionality

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,47 @@
 import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import axios from "axios";
+import Promotion from "./models/promotionModel";
 
 const app: Express = express();
 const port = 8080;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+dotenv.config();
+const emailApiUrl = process.env.EMAIL_API_URL as string;
+
+app.post("/promotion", async (req: Request, res: Response) => {
+  const promotion: Promotion = {
+    promotionId: req.body.promotionId,
+    promotionName: req.body.promotionName,
+    promotionDescription: req.body.promotionDescription,
+    promotionReceivers: req.body.promotionReceivers,
+  };
+
+  if (promotion) {
+    for (let i = 0; i < promotion.promotionReceivers.length; i++) {
+      try {
+        await axios.post(
+          emailApiUrl,
+          {
+            email: promotion.promotionReceivers[i],
+            subject: promotion.promotionName,
+            message: promotion.promotionDescription,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
+  res.status(200).send("OK");
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
---
+++ src/models/promotionModel.ts
@@ -0,0 +1,8 @@
+interface promotionModel {
+  promotionId: Number;
+  promotionName: String;
+  promotionDescription: String;
+  promotionReceivers: Array<String>;
+}
+
+export default promotionModel;
