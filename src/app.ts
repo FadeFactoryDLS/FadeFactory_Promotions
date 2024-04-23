64ee5206a75613a4cec002881203b07a2ff0070e
Snaflus <casp909i@edu.easj.dk>
2024-04-23 12:21:11 +0200

    project init

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import express, { Express, Request, Response } from "express";
+
+const app: Express = express();
+const port = 8080;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
